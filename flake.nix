235936c8e2b96f256505dadbec5eacacae8e240d
John Lewis <github@jlewis.sh>
2026-09-17 12:56:22 -0500

    re-init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = { nixpkgs, rust-overlay, devshell, flake-utils, ... }: 
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import rust-overlay)
+          devshell.overlays.default
+        ];
+      };
+
+      toolchain_fn = p: p.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+      };
+    in {
+      devShells.default = pkgs.devshell.mkShell {
+        packages = with pkgs; [ (toolchain_fn pkgs) gcc bacon ];
+        motd = "\n  Welcome to the {2}$(basename $PRJ_ROOT){reset} shell.\n";
+      };
+    });
+}
